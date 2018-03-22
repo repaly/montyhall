438a883c02f714229d13a3d645905b2b60cc25b8
repaly <dimsemin2008@yandex.ru>
2018-03-22 14:35:59 +0300

    добавил двери и кнопку

--- js/monty.js
+++ js/monty.js
@@ -1,7 +1,25 @@
 var door = {
-  prize: true;
+  opened: false,
+  prize: false
 };
+var door1 = door;
+var doors = [door, door, door];
+var $startButton = $("start");
 
-var door1;
-var door2;
-var door3;
+$(document).click(function (event) {
+  var $selectedElement = $(event.target);
+
+  if ( $selectedElement.hasClass("start") ) {
+    const prizeDoor = randomize(0, 2);
+    console.log(prizeDoor);
+    doors[prizeDoor].prize = true;
+    console.log(doors);
+  }
+});
+
+
+function randomize(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; // генерируется число включая минимум и максимум
+}
