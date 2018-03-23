09222763ec11a1f8977db0dac914775aecb82b86
repaly <dimsemin2008@yandex.ru>
2018-03-23 09:46:28 +0300

    дизайн и начало основной логики

--- js/monty.js
+++ js/monty.js
@@ -1,21 +1,64 @@
-var door1 = {
-  opened: false,
-  prize: false
-};
-var door2 = Object.assign({}, door1);
-var door3 = Object.assign({}, door1);
-var doors = [door1, door2, door3];
+var gameStarted = false;
 var $startButton = $(".start");
+var $doors = $(".door");
+var $doorsArray = $doors.toArray();
+
+for (var i = 0; i < $doorsArray.length; i++) {
+  $doorsArray[i].prize = false;
+  $doorsArray[i].open = function () {
+    $(this).hide();
+  }
+  $doorsArray[i].selected = false;
+}
 
 $(document).click(function (event) {
+
   var $selectedElement = $(event.target);
+
   if ( $selectedElement.hasClass("start") ) {
+
     const prizeDoor = randomize(0, 2);
-    doors[prizeDoor].prize = true;
+
+    $doorsArray[prizeDoor].prize = true;
     $startButton.hide("slow");
+    startGame();
+    $(".question").show(1000);
+  } else if ( $selectedElement.hasClass("door") ) {
+
+    if (gameStarted) {
+      $doors.off("mouseover");
+      $doors.off("mouseout");
+      $selectedElement[0].selected = true;
+      openOtherDoor();
+    }
   }
 });
 
+function startGame() {
+
+  gameStarted = true;
+
+  $doors.mouseover(function (event) {
+    $(this).children(".selection").show();
+  });
+
+  $doors.mouseout(function (event) {
+    $(this).children(".selection").hide();
+  });
+}
+
+function openOtherDoor() { // надо сделать так, чтобы убиралась дверь без приза
+
+  var $otherDoors = $doorsArray;
+  const openDoor = randomize(0, 1);
+
+  for (var i = 0; i < $otherDoors.length; i++) {
+    if ($otherDoors[i].selected === true) {
+      $otherDoors.splice($otherDoors[i], 1); //какая-то фигня, надо додумать
+    }
+  }
+  $otherDoors[openDoor].open();
+}
 
 function randomize(min, max) {
   min = Math.ceil(min);
